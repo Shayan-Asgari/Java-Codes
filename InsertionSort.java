c660b21c64ebeca0e597d0d8fc5ff8ae52834e9e
Shayan-Asgari <31261309+Shayan-Asgari@users.noreply.github.com>
2018-10-13 12:54:49 -0700

    Update InsertionSort.java
    Please consider the differences between the original and this, and let me know if anything is wrong with the changes I proposed in this code.

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,28 +1,31 @@
 // Java program for implementation of Insertion Sort
-class InsertionSort{
-    void sort(int arr[]){
+class InsertionSort
+{
+    void sort(int arr[])
+    {
         int n = arr.length;
-        for (int i=1; i<n; ++i){
+        for (int i=1; i<n; i++) 
+        {
             int key = arr[i];
             int j = i-1;
  
-            while (j>=0 && arr[j] > key){
-                arr[j+1] = arr[j];
-                j = j-1;
-            }
-            arr[j+1] = key;
+           while (j>=0 && arr[j] > key)
+           {
+                int temp = arr[j]; // save arr[j] into a temporary
+                arr[j] = arr[j+1] ; // [J+1] will always be key
+                arr[j+1] = temp; // arr[j+1] will be temporary
+                j--;
+           }
         }
     }
  
-    static void printArray(int arr[]){
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i] + " ");
- 
-        System.out.println();
+    static void printArray(int arr[])
+    {
+        System.out.println(Arrays.toString(arr));
     }
  
-    public static void main(String args[]){        
+    public static void main(String args[])
+    {        
         int arr[] = {12, 11, 13, 5, 6};
  
         InsertionSort ob = new InsertionSort();        
